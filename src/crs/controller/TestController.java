271c1acc3782fd9ee8d32e4376f3aa22bf6a2c4d
ahmeddddddd69 <ahmedh7155@gmail.com>
2025-11-09 16:40:42 +0800

    first commit test

---
+++ src/crs/controller/TestController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crs.controller;
+
+/**
+ *
+ * @author ahmed
+ */
+public class TestController {
+    public static void main(String[]args){
+        System.out.println("CR controller test work!");
+    }
+    
+}
