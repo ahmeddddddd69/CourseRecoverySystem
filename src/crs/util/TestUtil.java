776b8f150d6a60ed8b984e8098ea22909dbcdb49
ahmeddddddd69 <ahmedh7155@gmail.com>
2025-11-09 16:44:51 +0800

    2nd commit test

---
+++ src/crs/util/TestUtil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crs.util;
+
+/**
+ *
+ * @author ahmed
+ */
+public class TestUtil {
+    public static void main(String[]args){
+        System.out.println("CR controller test work!");
+    }
+    
+}
